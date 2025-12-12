7761ec7bd1c6aff72fdd04988fbd090a1063f865
Luis Miguel Fernández Costa <luismiguel.fernandez@murciaeduca.es>
2025-12-12 14:22:49 +0100

    trabajando en Manage Cities

--- src/app/search/search.ts
+++ src/app/search/search.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { WeatherData } from '../weather-data';
+import { UserPrefs } from '../user-prefs';
 
 @Component({
   selector: 'app-search',
@@ -10,11 +11,29 @@ import { WeatherData } from '../weather-data';
 export class Search {
 
   private data = inject(WeatherData)
+  private userPrefs = inject(UserPrefs)
+  private results:any[] = []
+
+  addCityToMyCities(id:string) {
+    this.userPrefs.addCityToMyCities(id)
+  }
+
+  isAlreadyInMyCities(id:string) {
+    return this.userPrefs.isAlreadyInMyCities(id)
+  }
+
+  getResults() {
+    return this.results
+  }
+
+  removeCityFromMyCities(id:string) {
+    this.userPrefs.removeCityFromMyCities(id)
+  }
 
   searchByName(pattern:string) {
     this.data.searchByName(pattern).subscribe(
-      data => {
-        
+      (data:any) => {
+        this.results = data.list
       }
     )
   }
--- src/app/user-prefs.ts
+++ src/app/user-prefs.ts
@@ -4,5 +4,24 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class UserPrefs {
+  private myCities: string[] = JSON.parse(localStorage.getItem('myCities') || "[]");
   
+  addCityToMyCities(id:string) {
+    if (this.myCities.indexOf(id) === -1) {
+      this.myCities.push(id);
+      localStorage.setItem('myCities', JSON.stringify(this.myCities));
+    }
+  }
+
+  isAlreadyInMyCities(id:string) {
+    return this.myCities.indexOf(id) !== -1;
+  }
+
+  removeCityFromMyCities(id:string) {
+    const index = this.myCities.indexOf(id);
+    if (index !== -1) {
+      this.myCities.splice(index, 1);
+      localStorage.setItem('myCities', JSON.stringify(this.myCities));
+    }
+  }
 }
